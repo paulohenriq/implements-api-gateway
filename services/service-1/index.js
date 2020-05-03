fa835d6ea99959be0256a6604500c6f8188406a6
Paulo Henrique <paulohenriq.dev@gmail.com>
2020-05-03 16:57:34 -0300

    feat: send all files to project

---
+++ services/service-1/index.js
@@ -0,0 +1,24 @@
+var Koa = require('koa');
+var Router = require('koa-router');
+var bodyParser = require('koa-body');
+
+var app = new Koa();
+var router = new Router();
+
+app.use(bodyParser({  
+  multipart: true,
+  urlencoded: true
+}));
+
+router.get('/welcome', (ctx, next) => {  
+  ctx.body = {
+    message: "Example microservice 1 - Welcome path"
+  };
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+
+app.listen(3000);
